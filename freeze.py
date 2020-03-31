1ddf0d18d2e510b45d6dbe092784420f18cc8d19
Kevin Coxe <kevcoxe@gmail.com>
2020-03-30 22:36:33 -0400

    update freeze

--- freeze.py
+++ freeze.py
@@ -1,14 +1,8 @@
 from flask_frozen import Freezer
 from app import app
-# from app.models import Stuff
+
 
 freezer = Freezer(app)
-
-
-# @freezer.register_generator
-# def api_get_stuff():
-#     for stuff in Stuff.query.all():
-#         yield stuff.json()
 
 
 if __name__ == '__main__':
