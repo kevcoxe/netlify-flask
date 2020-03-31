7bbf1c00c81ef1b267f946d11f0ddcfbb256904c
Kevin Coxe <kevcoxe@gmail.com>
2020-03-30 21:11:52 -0400

    add some api

--- myapp.py
+++ myapp.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
+stuff = [
+    {
+        'name': 'Kevin',
+        'age': 0000
+    }
+]
 
-@app.route("/")
+
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+@app.route('/api/get/stuff', methods=['GET'])
+def api_get_stuff():
+    print('getting stuff: {}'.format(stuff))
+    return jsonify(stuff)
 
 
 if __name__ == '__main__':
