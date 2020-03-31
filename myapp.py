bc34a2d59cfdc76b655a0c9439b63eb754b42d38
Kevin Coxe <kevcoxe@gmail.com>
2020-03-30 21:17:03 -0400

    add api for adding stuff

--- myapp.py
+++ myapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 stuff = [
@@ -13,9 +13,21 @@ stuff = [
 def home():
     return render_template('home.html')
 
+
 @app.route('/api/get/stuff', methods=['GET'])
 def api_get_stuff():
     print('getting stuff: {}'.format(stuff))
+    return jsonify(stuff)
+
+
+@app.route('/api/add/stuff', methods=['POST'])
+def api_add_stuff():
+    name = request.json.get('name')
+    age = request.json.get('age')
+
+    stuff.append({'name': name, 'age': age})
+
+    print('adding stuff: {}'.format(stuff))
     return jsonify(stuff)
 
 
